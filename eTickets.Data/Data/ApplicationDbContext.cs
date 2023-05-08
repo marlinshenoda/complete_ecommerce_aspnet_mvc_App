500a35945f3a3b60324ad86ee483b56b4927389c
marlinshenoda <105081672+marlinshenoda@users.noreply.github.com>
2023-05-08 13:48:03 +0200

    clean Architure

--- eTickets.Data/Data/ApplicationDbContext.cs
+++ eTickets.Data/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace eTickets.Web.Data
+namespace eTickets.Data.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- eTickets.Web/Program.cs
+++ eTickets.Web/Program.cs
@@ -1,4 +1,4 @@
-using eTickets.Web.Data;
+using eTickets.Data.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
