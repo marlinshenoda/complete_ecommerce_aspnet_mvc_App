9f4fcf14de66f2341f85101bd8c25e65643b1daf
marlinshenoda <105081672+marlinshenoda@users.noreply.github.com>
2023-05-08 14:40:42 +0200

    Entities

---
+++ eTickets.Core/Entities/MovieCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace eTickets.Core.Entities
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary,
+        Cartoon,
+        Horror
+    }
+}
--- eTickets.Data/Data/ApplicationDbContext.cs
+++ eTickets.Data/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using eTickets.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace eTickets.Data.Data
@@ -8,6 +9,21 @@ namespace eTickets.Data.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+            });
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.ActorId);
+
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
